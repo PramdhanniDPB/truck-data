7a06f1d36dba620e17358030bad7a724fbfa39ca
PramdhanniDPB <pramdpb@gmail.com>
2026-01-28 08:48:37 +0700

    Auto commit from flow.bat

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def fetch_url(url):
 
 # Main execution
 if __name__ == "__main__":
-    year = 2026
+    year = 2024
     url = "http://10.10.206.205/data/api/nopolactive?year={}".format(year)
     data = fetch_url(url)
 
